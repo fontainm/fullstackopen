c1a8ffea4c8c371572d70ef451c1f1d2bca08b5c
fontainm <fontain@th-brandenburg.de>
2024-02-04 14:36:33 +0100

    exercise 4.1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  if (!request.body.title) {
+    response.status(400).end()
+  }
+  const blog = new Blog(request.body)
+  console.log(request.body)
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
