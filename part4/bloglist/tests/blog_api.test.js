2a1a07ab73b5eb2d2f2b0a62b369382d8d0c63f6
Mathias Fontain <mathias.fontain@vol.at>
2024-02-06 09:27:52 +0100

    refactoring

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -10,61 +10,65 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('viewing blogs', () => {
+  test('blogs are returned as json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  expect(response.body).toHaveLength(2)
+    expect(response.body).toHaveLength(2)
+  })
+
+  test('blogs have an id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
-test('blogs have an id', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body[0].id).toBeDefined()
-})
+describe('creating a blog', () => {
+  test('blog can be created', async () => {
+    const blogsBefore = await api.get('/api/blogs')
+    expect(blogsBefore.body).toHaveLength(2)
 
-test('blog can be created', async () => {
-  const blogsBefore = await api.get('/api/blogs')
-  expect(blogsBefore.body).toHaveLength(2)
+    const newBlog = {
+      author: 'Test Author',
+      title: 'Test Title',
+      url: 'test.url',
+      likes: 5,
+    }
+    const response = await api.post('/api/blogs').send(newBlog)
+    expect(response.body.id).toBeDefined()
 
-  const newBlog = {
-    author: 'Test Author',
-    title: 'Test Title',
-    url: 'test.url',
-    likes: 5,
-  }
-  const response = await api.post('/api/blogs').send(newBlog)
-  expect(response.body.id).toBeDefined()
+    const blogsAfter = await api.get('/api/blogs')
+    expect(blogsAfter.body).toHaveLength(3)
+  })
 
-  const blogsAfter = await api.get('/api/blogs')
-  expect(blogsAfter.body).toHaveLength(3)
-})
+  test('blog without likes will have 0 likes', async () => {
+    const newBlog = {
+      author: 'Test Author',
+      title: 'Test Title',
+      url: 'test.url',
+    }
+    const response = await api.post('/api/blogs').send(newBlog)
+    expect(response.body.likes).toEqual(0)
+  })
 
-test('blog without likes will have 0 likes', async () => {
-  const newBlog = {
-    author: 'Test Author',
-    title: 'Test Title',
-    url: 'test.url',
-  }
-  const response = await api.post('/api/blogs').send(newBlog)
-  expect(response.body.likes).toEqual(0)
-})
+  test('blog without title cannot be created', async () => {
+    const newBlog = {
+      author: 'Test Author',
+      url: 'test.url',
+    }
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  })
 
-test('blog without title cannot be created', async () => {
-  const newBlog = {
-    author: 'Test Author',
-    url: 'test.url',
-  }
-  await api.post('/api/blogs').send(newBlog).expect(400)
-})
-
-test('blog without url cannot be created', async () => {
-  const newBlog = {
-    title: 'Test Title',
-    author: 'Test Author',
-  }
-  await api.post('/api/blogs').send(newBlog).expect(400)
+  test('blog without url cannot be created', async () => {
+    const newBlog = {
+      title: 'Test Title',
+      author: 'Test Author',
+    }
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  })
 })
 
 describe('updating a blog', () => {
@@ -72,13 +76,15 @@ describe('updating a blog', () => {
     const blogsAtStart = await helper.blogsInDb()
     const blogToUpdate = blogsAtStart[0]
     blogToUpdate.likes = 100
-    const response = await api.put(`/api/blogs/${blogToUpdate.id}`).send(blogToUpdate)
+    const response = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(blogToUpdate)
     expect(response.body.likes).toEqual(100)
     expect(response.status).toBe(200)
   })
 })
 
-describe('deletion of a blog', () => {
+describe('deleting a blog', () => {
   test('succeeds with status code 204 if id is valid', async () => {
     const blogsAtStart = await helper.blogsInDb()
     const blogToDelete = blogsAtStart[0]
