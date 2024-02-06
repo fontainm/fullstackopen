ef03774e10ea60d786e77d955b7d1175dd7af62d
Mathias Fontain <mathias.fontain@vol.at>
2024-02-06 11:26:42 +0100

    exercise 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+  })
   response.json(blogs)
 })
 
@@ -11,9 +15,14 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end()
   }
 
+  const user = await User.findById(request.body.user)
+
   const blog = new Blog(request.body)
-  const result = await blog.save()
-  response.status(201).json(result)
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.put('/:id', async (request, response) => {
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,7 +3,11 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', {
+    url: 1,
+    author: 1,
+    title: 1,
+  })
   response.json(users)
 })
 
@@ -11,7 +15,9 @@ usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
   if (!password || password.length < 3) {
-    return response.status(400).json({ error: 'password must be at least 3 characters' })
+    return response
+      .status(400)
+      .json({ error: 'password must be at least 3 characters' })
   }
 
   const saltRounds = 10
