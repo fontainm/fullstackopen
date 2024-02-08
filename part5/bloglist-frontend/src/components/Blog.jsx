ecf30bdeeee03a413946b0387ab0e007d770bbb4
Mathias Fontain <mathias.fontain@vol.at>
2024-02-08 11:20:36 +0100

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const blogStyle = {
+    padding: 10,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  if (!blogVisible) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title}
+          <button onClick={() => setBlogVisible(true)}>view</button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={() => setBlogVisible(false)}>hide</button>
+      </div>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.author}</div>
+    </div>
+  )
+}
 
 export default Blog
