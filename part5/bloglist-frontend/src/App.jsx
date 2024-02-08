bd93d8e172918b1b05b5fac68e05462e4f21fb1e
Mathias Fontain <mathias.fontain@vol.at>
2024-02-08 09:44:48 +0100

    exercise 5.3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -9,6 +9,10 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   const loginForm = () => (
     <>
       <h2>Log in to application</h2>
@@ -36,13 +40,50 @@ const App = () => {
     </>
   )
 
-  const blogList = () => (
+  const blogForm = () => (
     <>
-      <h2>blogs</h2>
       <p>
         {user.name} logged in
         <button onClick={handleLogout}>logout</button>
       </p>
+
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          title
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url
+          <input
+            type="text"
+            value={url}
+            name="URL"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
+
+  const blogList = () => (
+    <>
+      <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
@@ -82,13 +123,37 @@ const App = () => {
     }
   }
 
-  const handleLogout = async (event) => {
+  const handleLogout = (event) => {
     event.preventDefault()
     setUser(null)
     window.localStorage.removeItem('blogUser')
   }
 
-  return <div>{user === null ? loginForm() : blogList()}</div>
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    console.log('create', title, author, url)
+    try {
+      const newBlog = {
+        title,
+        author,
+        url,
+      }
+      const response = await blogService.create(newBlog)
+      setBlogs(blogs.concat(response))
+    } catch (exception) {
+      setErrorMessage('Invalid')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  return (
+    <div>
+      {user === null ? loginForm() : blogForm()}
+      {blogList()}
+    </div>
+  )
 }
 
 export default App
