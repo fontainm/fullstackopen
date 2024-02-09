f5198508e04697aedfe931eaad81c1baa23c7e4d
Mathias Fontain <mathias.fontain@vol.at>
2024-02-09 12:50:50 +0100

    exercise 5.16

--- part5/bloglist-frontend/src/components/BlogForm.test.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.test.jsx
@@ -16,16 +16,13 @@ test('renders content', async () => {
 
   const userE = userEvent.setup()
   const inputTitle = screen.getByPlaceholderText('Title')
-  const inputAuthor = screen.getByPlaceholderText('Author')
   const inputURL = screen.getByPlaceholderText('URL')
   const button = screen.getByText('create')
   await userE.type(inputTitle, 'Blog title')
-  await userE.type(inputAuthor, 'Blog author')
   await userE.type(inputURL, 'www.blog.url')
   await userE.click(button)
 
   expect(createBlog.mock.calls).toHaveLength(1)
   expect(createBlog.mock.calls[0][0].title).toBe('Blog title')
-  expect(createBlog.mock.calls[0][0].author).toBe('Blog author')
   expect(createBlog.mock.calls[0][0].url).toBe('www.blog.url')
 })
