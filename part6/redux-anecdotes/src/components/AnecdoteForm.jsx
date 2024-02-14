b84c702d63773e7bfd00cb804f9eb150b4f8d464
Mathias Fontain <mathias.fontain@vol.at>
2024-02-14 10:39:52 +0100

    exercise 6.15

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -5,14 +5,17 @@ import {
   showNotification,
 } from '../reducers/notificationReducer'
 
+import anecdoteService from '../services/anecdotes'
+
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(showNotification(`Created new anecdote: '${content}'`))
     setTimeout(() => dispatch(hideNotification()), 5000)
   }
