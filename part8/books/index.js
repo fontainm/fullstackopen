954b71b1ae3c44e96f249ae229474d88a0479bd3
Mathias Fontain <mathias.fontain@vol.at>
2024-02-26 11:25:59 +0100

    exercise 8.3

--- part8/books/index.js
+++ part8/books/index.js
@@ -87,11 +87,19 @@ const typeDefs = `
     id: ID!
     genres: [String]
   }
+
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
   
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -100,6 +108,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => {
+      const bookCount = books.filter((book) => book.author === root.name)
+      return bookCount.length
+    },
   },
 }
 
