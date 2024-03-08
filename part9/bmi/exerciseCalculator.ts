b18118e3591986f49f3957c62bff3040f196c2d2
Mathias Fontain <mathias.fontain@vol.at>
2024-03-08 11:04:03 +0100

    ex 9.3

--- part9/bmi/exerciseCalculator.ts
+++ part9/bmi/exerciseCalculator.ts
@@ -1,3 +1,5 @@
+import { isNotNumber } from './utils'
+
 interface Exercise {
   periodLength: number
   trainingDays: number
@@ -8,16 +10,55 @@ interface Exercise {
   average: number
 }
 
-const calculateExercises = (hours: Array<number>): Exercise => {
+const calculateExercises = (hours: Array<number>, target: number): Exercise => {
+  const periodLength = hours.length
+  const trainingDays = hours.filter((hour) => hour > 0).length
+  const sum = hours.reduce((a, b) => a + b, 0)
+  const average = sum / periodLength
+  const success = average > target
+  let rating = 0
+  let ratingDescription = ''
+  const diff = average - target
+  if (diff > 0) {
+    rating = 1
+    ratingDescription = 'thats good!'
+  } else if (diff > -0.5) {
+    rating = 2
+    ratingDescription = 'not too bad but could be better'
+  } else {
+    rating = 3
+    ratingDescription = 'do more!'
+  }
   return {
-    periodLength: 7,
-    trainingDays: 5,
-    success: false,
-    rating: 2,
-    ratingDescription: 'not too bad but could be better',
-    target: 2,
-    average: 1.9285714285714286,
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]))
+const target: number = Number(process.argv[2])
+const hours: Array<number> = []
+
+process.argv.map((val, index) => {
+  if (index > 2) {
+    if (isNotNumber(Number(val))) {
+      throw new Error('Invalid argument, must provide only numbers')
+    }
+    hours.push(Number(val))
+  }
+})
+
+try {
+  console.log(calculateExercises(hours, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+
+  if (error instanceof Error) {
+    errorMessage += error.message
+  }
+  console.log(errorMessage)
+}
