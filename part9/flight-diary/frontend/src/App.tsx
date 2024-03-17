ccf21151ca4a05e2aa950b067f5765928d931714
Mathias Fontain <mathias.fontain@vol.at>
2024-03-17 11:27:42 +0100

    ex 9.17

--- part9/flight-diary/frontend/src/App.tsx
+++ part9/flight-diary/frontend/src/App.tsx
@@ -11,6 +11,10 @@ interface DiaryEntry {
 
 function App() {
   const [diaryEntries, setDiaryEntries] = useState<DiaryEntry[]>([]);
+  const [newDate, setNewDate] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newComment, setNewComment] = useState('');
 
   useEffect(() => {
     axios
@@ -18,13 +22,67 @@ function App() {
       .then((response) => {
         setDiaryEntries(response.data);
       });
-  });
+  }, []);
+
+  const diaryEntryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const diaryEntryToAdd = {
+      id: diaryEntries.length + 1,
+      date: newDate,
+      visibility: newVisibility,
+      weather: newWeather,
+      comment: newComment,
+    };
+
+    axios
+      .post<DiaryEntry>('http://localhost:3000/api/diaries', diaryEntryToAdd)
+      .then((response) => {
+        setDiaryEntries(diaryEntries.concat(response.data));
+      });
+
+    setNewDate('');
+    setNewVisibility('');
+    setNewWeather('');
+    setNewComment('');
+  };
 
   return (
     <>
+      <h2>Add new entry</h2>
+      <form onSubmit={diaryEntryCreation}>
+        <div>
+          date
+          <input
+            value={newDate}
+            onChange={(event) => setNewDate(event.target.value)}
+          />
+        </div>
+        <div>
+          visibility
+          <input
+            value={newVisibility}
+            onChange={(event) => setNewVisibility(event.target.value)}
+          />
+        </div>
+        <div>
+          weather
+          <input
+            value={newWeather}
+            onChange={(event) => setNewWeather(event.target.value)}
+          />
+        </div>
+        <div>
+          comment
+          <input
+            value={newComment}
+            onChange={(event) => setNewComment(event.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
       <h2>Diary entries</h2>
       {diaryEntries.map((entry) => (
-        <div>
+        <div key={entry.id}>
           <h3>{entry.date}</h3>
           <p>Visibility: {entry.visibility}</p>
           <p>Weather: {entry.weather}</p>
